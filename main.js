13c9bae0f506aed2dcdeb1a667744121018ab00e
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-08-18 19:29:40 +0530

    adding animation

---
+++ main.js
@@ -0,0 +1,26 @@
+gsap.from(".rtleft", {
+  opacity: 0,
+  y: -300,
+  duration: 1.2,
+});
+gsap.from(".rtright", {
+  opacity: 0,
+  x: 300,
+  duration: 1.2,
+});
+gsap.from(".rbottom", {
+  opacity: 0,
+  y: 300,
+  duration: 1.2,
+});
+
+gsap.from(".lbottom", {
+  opacity: 0,
+  scale: 0.1,
+  duration: 0.8,
+});
+gsap.from(".ltop", {
+  opacity: 0,
+  y: -100,
+  duration: 0.8,
+});
